0d4abb7fc6de3ee61b45748f73e8b5d22f69827a
unknown <pjjoyce@KTH-6286.ug.kth.se>
2015-09-23 13:03:07 +0200

    database persist...

--- wsgi/myproject/myproject/settings.py
+++ wsgi/myproject/myproject/settings.py
@@ -119,7 +119,9 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
-
+STATICFILES_DIRS = (
+    ("uploaded_files", os.path.join(DATA_DIR, "uploaded_files")),
+)
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(WSGI_DIR, 'static')
-MEDIA_ROOT = os.path.join(BASE_DIR, "static/uploaded_files")
+MEDIA_ROOT = os.path.join(DATA_DIR, "uploaded_files")
