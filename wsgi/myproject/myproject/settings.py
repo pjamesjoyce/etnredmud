e2bd724e3661396918c36b30dfc28fa941427602
unknown <pjjoyce@KTH-6286.ug.kth.se>
2015-09-23 12:12:18 +0200

    attempted db bugfix

--- wsgi/myproject/myproject/settings.py
+++ wsgi/myproject/myproject/settings.py
@@ -14,7 +14,8 @@ DJ_PROJECT_DIR = os.path.dirname(__file__)
 BASE_DIR = os.path.dirname(DJ_PROJECT_DIR)
 WSGI_DIR = os.path.dirname(BASE_DIR)
 REPO_DIR = os.path.dirname(WSGI_DIR)
-DATA_DIR = os.environ.get('OPENSHIFT_DATA_DIR', BASE_DIR)
+#DATA_DIR = os.environ.get('OPENSHIFT_DATA_DIR', BASE_DIR)
+DATA_DIR = BASE_DIR
 
 import sys
 sys.path.append(os.path.join(REPO_DIR, 'libs'))
