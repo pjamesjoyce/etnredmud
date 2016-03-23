5ab999820621d206f10f2f9dfab295c951c92e2b
unknown <pjjoyce@KTH-6286.ug.kth.se>
2016-03-23 15:55:51 +0100

    lxml setup.py attempt

--- setup.py
+++ setup.py
@@ -18,6 +18,7 @@ setup(
     # GETTING-STARTED: define required django version:
     install_requires=[
         'Django==1.8.4'
+        'lxml'
     ],
     dependency_links=[
         'https://pypi.python.org/simple/django/'
