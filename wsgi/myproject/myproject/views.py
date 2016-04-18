b78f60cea37bd00139b6eb53dd782a22fa9a8a2c
unknown <pjjoyce@KTH-6286.ug.kth.se>
2016-04-18 16:38:56 +0200

    Blog on front page fix

--- wsgi/myproject/myproject/views.py
+++ wsgi/myproject/myproject/views.py
@@ -24,7 +24,7 @@ def home(request):
     args['homeblock3']=homeblock3
 
     args['latestNews'] = redmudLatest("http://etn.redmud.org/news/")
-    args['latestBlog'] = redmudLatest("http://etn.redmud.org/blog/")
+    args['latestBlog'] = redmudLatestBlog("http://etn.redmud.org/blog/")
 
     return render(request, 'home.html', args)
 
@@ -42,6 +42,30 @@ def redmudLatest(url):
     titleXPath = '//article/h2/a/text()'
     linkXPath = '//article/h2/a/@href'
     imgXpath = '//article/div/a/img/@src'
+    titles = tree.xpath(titleXPath)
+    links = tree.xpath(linkXPath)
+    images = tree.xpath(imgXpath)
+
+    data = []
+    for i, title in enumerate(titles):
+        data.append({'title':title, 'link':links[i], 'img':images[i]})
+
+    return data
+
+def redmudLatestBlog(url):
+
+    page = requests.get(url)
+    encoding = page.encoding
+    print encoding
+
+    if encoding != 'utf-8':
+        encpage = page.content.decode(encoding, 'replace').encode('utf-8')
+
+    tree = html.fromstring(encpage)
+
+    titleXPath = '//article/div[2]/h3/a/text()'
+    linkXPath = '//article/div[2]/h3/a/@href'
+    imgXpath = '//article/div[1]/a/img/@src'
     titles = tree.xpath(titleXPath)
     links = tree.xpath(linkXPath)
     images = tree.xpath(imgXpath)
